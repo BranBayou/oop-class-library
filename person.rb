02e6607d15008b4cbcf8c1ae7ebd8298ea789008
BranBayou <berhan.baye@gmail.com>
2023-07-04 10:56:50 +0300

    Add classes

---
+++ person.rb
@@ -0,0 +1,25 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, parent_permission: true, name: 'Unknown')
+    @id = generate_id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
+  def of_age?
+    @age >= 18
+  end
+
+  private
+
+  def generate_id
+    rand(1000..9999)
+  end
+end
